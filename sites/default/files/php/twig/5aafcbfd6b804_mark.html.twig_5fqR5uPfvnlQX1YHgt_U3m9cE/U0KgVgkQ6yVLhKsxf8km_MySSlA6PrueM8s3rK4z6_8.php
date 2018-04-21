00b666c364ae0935f32c489075c0d6266ed09790
Fer <alt.nando@gmail.com>
2018-04-20 18:37:11 -0600

    primer commit

---
+++ sites/default/files/php/twig/5aafcbfd6b804_mark.html.twig_5fqR5uPfvnlQX1YHgt_U3m9cE/U0KgVgkQ6yVLhKsxf8km_MySSlA6PrueM8s3rK4z6_8.php
@@ -0,0 +1,90 @@
+<?php
+
+/* core/themes/classy/templates/content/mark.html.twig */
+class __TwigTemplate_ce4cc37dbfaa48e082004ef113fe755828ea1013e22b9e3176aa63167e151607 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 14);
+        $filters = array("t" => 16);
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('if'),
+                array('t'),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 14
+        if (($context["logged_in"] ?? null)) {
+            // line 15
+            echo "  ";
+            if ((($context["status"] ?? null) === constant("MARK_NEW"))) {
+                // line 16
+                echo "    <span class=\"marker\">";
+                echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("New")));
+                echo "</span>
+  ";
+            } elseif ((            // line 17
+($context["status"] ?? null) === constant("MARK_UPDATED"))) {
+                // line 18
+                echo "    <span class=\"marker\">";
+                echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("Updated")));
+                echo "</span>
+  ";
+            }
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/content/mark.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  55 => 18,  53 => 17,  48 => 16,  45 => 15,  43 => 14,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "core/themes/classy/templates/content/mark.html.twig", "C:\\xampp\\htdocs\\drupal8\\core\\themes\\classy\\templates\\content\\mark.html.twig");
+    }
+}
